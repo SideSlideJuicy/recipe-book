a4b78cb01520c401622fc248d0e5ca23902505db
Jussi Nevalainen <jussi.nevalainen89@gmail.com>
2023-11-13 21:31:39 +0200

    Updated recipe book

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { useParams, Link, useLocation } from 'react-router-dom';
+import { useParams, Link, useLocation, useNavigate } from 'react-router-dom';
 import './RecipeDetail.css';
 
 const RecipeDetail = () => {
   const { id } = useParams();
   const location = useLocation();
+  const navigate = useNavigate();
+
   const recipe = location.state;
 
   console.log('ID from URL:', id);
   console.log('Current Recipe:', recipe);
+
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/api/recipes/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        console.log('Recipe deleted successfully');
+        // Redirect to the recipe list after deletion
+        navigate('/');
+      } else {
+        console.error('Failed to delete recipe');
+      }
+    } catch (error) {
+      console.error('Error deleting recipe:', error);
+    }
+  };
 
   if (!recipe || recipe._id !== id) {
     return <div>Recipe not found.</div>;
@@ -26,7 +49,13 @@ const RecipeDetail = () => {
         <h3>Instructions</h3>
         <p className="recipe-content">{recipe.instructions}</p>
       </div>
-      <Link to="/" className="back-link">Back to Recipe List</Link>
+      {/* Delete button */}
+      <button onClick={handleDelete} className="delete-button">
+        Delete Recipe
+      </button>
+      <Link to="/" className="back-link">
+        Back to Recipe List
+      </Link>
     </div>
   );
 };
